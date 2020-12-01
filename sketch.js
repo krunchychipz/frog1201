6af88cd008c83096bb3d501c12f1140a0778cb24
krunchychipz <71304476+krunchychipz@users.noreply.github.com>
2020-12-01 11:46:42 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://krunchychipz.github.io/frog1201/model.json';
 
 
 function preload() {
